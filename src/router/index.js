01dea945b58795d9541d9928909d048127bdfaaa
Praise <p.o.adebayo218@gmail.com>
2023-07-17 14:04:17 +0100

    optimized the code

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,21 @@
+import { defineAsyncComponent } from 'vue';
+
 import { createRouter, createWebHistory } from 'vue-router'
-import CoachesDetails from '../views/coaches/CoachesDetails.vue';
+// import CoachesDetails from '../views/coaches/CoachesDetails.vue';
 import CoachesList from '../views/coaches/CoachesList.vue';
-import CoachesRegister from '../views/coaches/CoachesRegistration.vue';
-import ContactCoach from '../views/requests/ContactCoach.vue';
-import RequestReceived from '../views/requests/RequestReceived.vue';
-import NotFound from '../views/NotFound.vue';
-import UserAuth from '../views/auth/UserAuth.vue'
-import store from '../store/modules/index'
+// import CoachesRegister from '../views/coaches/CoachesRegistration.vue';
+// import ContactCoach from '../views/requests/ContactCoach.vue';
+// import RequestReceived from '../views/requests/RequestReceived.vue';
+// import NotFound from '../views/NotFound.vue';
+// import UserAuth from '../views/auth/UserAuth.vue'
+import store from '../store/modules/index';
+
+const CoachesDetails = () => import('../views/coaches/CoachesDetails.vue');
+const CoachesRegister = () => import('../views/coaches/CoachesRegistration.vue');
+const ContactCoach = () => import('../views/requests/ContactCoach.vue');
+const RequestReceived  = () => import('../views/requests/RequestReceived.vue');
+const UserAuth = () => import('../views/auth/UserAuth.vue');
+const NotFound = () => import('../views/NotFound.vue');
 
 const routes = [
   { path: '/', redirect: '/coaches' },
